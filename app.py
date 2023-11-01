3e56222726980b8926b505b3083ffdf2164296c9
daniloui001 <daniel.louie@stonybrook.edu>
2023-10-31 21:49:15 -0400

    created app.py

---
+++ app.py
@@ -0,0 +1,6 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+from matplotlib.figure import Figure
+
