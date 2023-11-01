5f9444e8b5e6ad7b42e1961896a3c5c18afbd526
daniloui001 <daniel.louie@stonybrook.edu>
2023-11-01 15:08:27 -0400

    cleaned data

--- app.py
+++ app.py
@@ -4,3 +4,8 @@ import matplotlib.pyplot as plt
 from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
 from matplotlib.figure import Figure
 
+df = pd.read_csv('https://raw.githubusercontent.com/daniloui001/Correlation/main/movies.csv')
+
+#Overview
+
+df.head()
